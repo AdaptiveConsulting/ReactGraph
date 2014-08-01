b00fbabbfd696a1eb8e9f116a22d2dda8bcfa4f2
Jake Ginnivan <jake@ginnivan.net>
2014-08-01 08:47:25 +0100

    Moved class to it's own file

--- src/ReactGraph.Tests/NotifyPropertyChangedTests.cs
+++ src/ReactGraph.Tests/NotifyPropertyChangedTests.cs
@@ -81,20 +81,4 @@ namespace ReactGraph.Tests
             updatedObjects.ShouldContain(four);
         }
     }
-
-    class SimpleWithNotification : NotifyPropertyChanged
-    {
-        private int value;
-
-        public int Value
-        {
-            get { return value; }
-            set
-            {
-                if (value == this.value) return;
-                this.value = value;
-                OnPropertyChanged("Value");
-            }
-        }
-    }
 }
